b9ff460aa62f859ff1d25c07f48a7cbf8cd2c2ae
Martin Wickham <spexguy070@gmail.com>
2015-03-01 15:44:36 -0600

    Fix color.cpp namespace issue

--- src/color.cpp
+++ src/color.cpp
@@ -1,9 +1,10 @@
 /** Copyright (C) 2013 David Braam - Released under terms of the AGPLv3 License */
 #include "color.h"
 
-using namespace Cura;
+namespace cura {
 
 void flatColorCallback(ClipperLib::IntPoint& z1, ClipperLib::IntPoint& z2, ClipperLib::IntPoint& pt) {
     pt.Z = z1.Z;
 }
 
+}

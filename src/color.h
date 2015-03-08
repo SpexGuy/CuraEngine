8cb16bf7ffbd81efa0a1565e95455616773e99ba
Martin Wickham <spexguy070@gmail.com>
2015-03-08 15:16:29 -0500

    Add color caching (probably)

--- src/color.h
+++ src/color.h
@@ -2,6 +2,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include <set>
 #include "../libs/clipper/clipper.hpp"
 
 namespace cura {
@@ -15,10 +16,37 @@ public:
 
     Color() : r(0), g(0), b(0) {}
     Color(float red, float green, float blue) : r(red), g(green), b(blue) {}
-
 };
 
 void flatColorCallback(ClipperLib::IntPoint& z1, ClipperLib::IntPoint& z2, ClipperLib::IntPoint& pt);
+
+struct ColorComparator {
+	bool operator() (const Color& self, const Color& other) {
+    	if (self.r != other.r)
+    		return r < other.r;
+    	if (self.g != other.g)
+    		return g < other.g;
+    	if (self.b != other.b)
+    		return b < other.b;
+    	return false;
+	}
+};
+
+class ColorCache
+{
+public:
+	static ColorCache& inst();
+	const Color* getColor(const float r, const float g, const float b);
+
+private:
+	typedef std::set<Color, ColorComparator> ColorSet;
+	typedef ColorSet::iterator ColorIterator;
+	
+	static ColorCache instance;
+
+	ColorSet cache;
+	const ColorIterator& createColor(const Color &c);
+};
 
 }//namespace cura
 
